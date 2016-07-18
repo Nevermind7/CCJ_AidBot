8f85bc6ed273926842abb939ad62655c4fa3fcff
Benjamin Eßer <esser@anvo-systems-dresden.com>
2016-07-18 17:21:34 +0200

    added some more structure and additional keywords

--- bot.py
+++ bot.py
@@ -7,13 +7,32 @@ class Bot:
         self.user_agent = 'CCJ_AidBot v.{} by /u/individual_throwaway'.format(ver)
         self.r = praw.Reddit(self.user_agent)
         self.o = OAuth2Util.OAuth2Util(self.r)
-        self.keywords_singular = keywords_singular
-        self.keywords_plural = keywords_plural
+        self.kw_singular = keywords_singular
+        self.kw_plural = keywords_plural
+        self.submissions = None
+        self.found_kw = {}
+    
+    def _get_recent_submissions(self):
+        self.o.refresh()
+        ccj = self.r.get_subreddit('climbingcirclejerk')
+        self.submissions = ccj.get_new(limit=10)
+    
+    def _parse_OP(self, submission):
+        pass
         
-    def _look_for_keywords(self, comment):
-        found = []
-        return found
-        
+    def _parse_comments(self, comments):
+        pass
+    
+    def _reply(self, submission, found):
+        pass
+    
+    def run(self):
+        self.submissions = self._get_recent_submissions()
+        for submission in self.submissions:
+            self._parse_OP
+            self._parse_comments
+        if self.found_kw:
+            self._reply()
         
 VERSION = '0.1'
 KEYWORDS_SINGULAR = ['chalk',
@@ -21,7 +40,8 @@ KEYWORDS_SINGULAR = ['chalk',
                      'honnold',
                      'gear',
                      'belay',
-                     'aid']
+                     'aid',
+                     'harness']
 
 KEYWORDS_PLURAL = ['quickdraw',
                    'anchor',
@@ -35,11 +55,16 @@ KEYWORDS_PLURAL = ['quickdraw',
                    'ascender',
                    'belayer',
                    'jug',
-                  'pinch']
+                   'pinch',
+                   'crimp',
+                   'carabiner',
+                   'biner',
+                   ]
 
 
 def main():
     aid = Bot(VERSION, KEYWORDS_SINGULAR, KEYWORDS_PLURAL)
+    aid.run()
 
 if __name__ == '__main__':
     main()
