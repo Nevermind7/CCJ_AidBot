c080d13e38bf9e0f1743131180c003d2759a594b
Benjamin Eßer <esser@anvo-systems-dresden.com>
2016-07-19 13:51:27 +0200

    some restructuring

--- bot.py
+++ bot.py
@@ -1,38 +1,36 @@
 import praw
 import OAuth2Util
 
-class Bot:
+class AidBot:
     
-    def __init__(self, ver, keywords_singular, keywords_plural):
+    def __init__(self, ver, kw_singular, kw_plural):
         self.user_agent = 'CCJ_AidBot v.{} by /u/individual_throwaway'.format(ver)
         self.r = praw.Reddit(self.user_agent)
         self.o = OAuth2Util.OAuth2Util(self.r)
-        self.kw_singular = keywords_singular
-        self.kw_plural = keywords_plural
-        self.submissions = None
+        self.kw_singular = kw_singular
+        self.kw_plural = kw_plural
         self.found_kw = {}
+        #self.comments = None
+        self.replied_to = self._get_replied_list()
     
-    def _get_recent_submissions(self):
+    def _get_replied_list(self):
+        """Makes sure we don't reply to the same comment twice."""
+    
+    def _get_comments(self):
         self.o.refresh()
         ccj = self.r.get_subreddit('climbingcirclejerk')
-        self.submissions = ccj.get_new(limit=10)
-    
-    def _parse_OP(self, submission):
-        pass
+        self.comments = [x for x in ccj.get_comments()]
         
     def _parse_comments(self, comments):
+        """Look for keywords in comments, reply on the first hit."""
         pass
     
     def _reply(self, submission, found):
         pass
     
     def run(self):
-        self.submissions = self._get_recent_submissions()
-        for submission in self.submissions:
-            self._parse_OP
-            self._parse_comments
-        if self.found_kw:
-            self._reply()
+        self._get_comments()
+        print(len(self.comments))
         
 VERSION = '0.1'
 KEYWORDS_SINGULAR = ['chalk',
@@ -58,12 +56,11 @@ KEYWORDS_PLURAL = ['quickdraw',
                    'pinch',
                    'crimp',
                    'carabiner',
-                   'biner',
-                   ]
+                   'biner']
 
 
 def main():
-    aid = Bot(VERSION, KEYWORDS_SINGULAR, KEYWORDS_PLURAL)
+    aid = AidBot(VERSION, KEYWORDS_SINGULAR, KEYWORDS_PLURAL)
     aid.run()
 
 if __name__ == '__main__':
